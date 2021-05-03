c23c4d0687a184b5178baacd7cc5bf49fe2b2972
MintSoftnix <darunporn.san@gmail.com>
2021-05-03 11:19:42 +0700

    [#1]hover menu

--- src/modules/apps/Sidebar.tsx
+++ src/modules/apps/Sidebar.tsx
@@ -31,16 +31,17 @@ const Sidebar= () => {
     return(
         <>
         <div className ="logosidebar">
-            <img src="logods.png" width="100" height ="100"/>
+            <img className="logo" src="logods.png" />
         </div>
         <div className = "allMenu">
             {memu2.map((p:any)=>{
-                return (<a href={p.to}>
-                    <div className = "nameMenu py-3">
-                        <i className="material-icons md-24" style={{verticalAlign:'top'}}>{p.icon}</i>
-                            <label className = "ml-2 mb-0" style={{cursor:'pointer'}}>{p.name}</label>
-                       
-                    </div> </a>
+                return (
+                    <a href={p.to} style={{textDecoration:'none'}}>
+                        <div className = "nameMenu py-3">
+                            <i className="material-icons md-24" style={{verticalAlign:'top'}}>{p.icon}</i>
+                            <label className = "menuName ml-2 mb-0" style={{cursor:'pointer'}}>{p.name}</label>
+                        </div> 
+                    </a>
                    
                 )
             })}
