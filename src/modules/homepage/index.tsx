30799c234403502ad0e47d8059dd6273817d2704
MintSoftnix <darunporn.san@gmail.com>
2021-05-17 20:21:21 +0700

    [#4]Homepage Design

--- src/modules/homepage/index.tsx
+++ src/modules/homepage/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {
+    useEffect 
+} from 'react'
 import { withTranslation, WithTranslation } from 'react-i18next';
 import { useTranslation } from 'react-i18next';
 
@@ -8,10 +11,48 @@ interface IHomePage{
 }
 const HomePage:React.FC<WithTranslation> = props =>{
     const { t } = useTranslation();    
+    var div = document.getElementById('sidebar')
+    let image = document.getElementById('imgHome')
+
+    if(div !== null){
+        div.addEventListener("mouseover", function( event ) {
+            image!.style.width = '250px'
+            image!.style.height = '250px'
+
+            document.getElementById('imageHome')!.style.transform = "translate(13%, -70%)";
+        });
+
+          div.addEventListener("mouseout", function( event ) {
+            image!.style.width = '300px'
+            image!.style.height = '300px'
+
+            document.getElementById('imageHome')!.style.transform = "translate(-35%, -70%)";
+          });
+    }
+ 
     return(
-        <div>
-            {t('menu_home')}
-        </div>
+        <>
+            <div className="row homepage" id ="homepage">
+                <div className="col helloHome" id = "helloHome">
+                    <label>hello</label>
+                    <div className="iconHome">
+                    <a href="https://github.com/darunporn-san" target="_blank">
+                        <img src="GitHub-Mark-64px.png" width="50px" height="50px"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/darunporn-santisawaddiwong-4a0721128/" target="_blank">
+                        <img src="linkedin.png" width="60px" height="60px"/>
+                    </a>
+                    </div>
+                </div>
+                <div className="imageHome" id ="imageHome">
+                    <img src="homepic.jpg" id="imgHome"/>
+                </div>
+                <div className="col introduce">
+                    right
+                </div>
+            </div>
+            {/* {t('menu_home')} */}
+        </>
     )
 }
 export default withTranslation()(HomePage)
--- src/shared/lib.ts
+++ src/shared/lib.ts
@@ -10,7 +10,32 @@ const resources = {
           "menu_expirence": "Expirence",
           "menu_contact": "Contact us",
           "address_header":"Address",
-          "address":"1028 Chodsathidtham apartment, Hemwong 1, Dindaeng, Dindaeng, Bangkok 10400"
+          "address":"1028 Chodsathidtham apartment, Hemwong 1, Dindaeng, Dindaeng, Bangkok 10400",
+          "work_header": "Work Experience",
+          "education_header" :  "Education",
+          "profile":{
+            "name":"Darunporn Santisawaddiwong (Mint)",
+            "sex":"female",
+            "age":"26 years",
+            "birthday_name":"Birthday",
+            "birthday":"30/01/1995"
+          },
+          "education":{
+            "university":"Mahidol University",
+            "faculty":"Information and Communi",
+            "school":"Wichienmatu School",
+            "mathayom":"Science-Math(SMA)",
+            "gpa_name":"GPA"
+          },
+          "home":{
+            "start":"Hi, I am",
+            "second":"I am a"
+          },
+          "work_details":{
+            "position":"Frontend Developer",
+            "first_office":"2Bsimple",
+            "second_office":"Softnix Technology",
+          }
         }
     },
     th: {
@@ -21,7 +46,32 @@ const resources = {
           "menu_expirence": "ประสบการณ์",
           "menu_contact": "ติดต่อเรา",
           "address_header":"ที่อยู่",
-          "address":"1028 โชติสถิตธรรมอพาร์ทเมนท์ ซ.เหมวงศ์ 1 แขวงดินแดง เขตดินแดง กรุงเทพมหานคร 10400"
+          "address":"1028 โชติสถิตธรรมอพาร์ทเมนท์ ซ.เหมวงศ์ 1 แขวงดินแดง เขตดินแดง กรุงเทพมหานคร 10400",
+          "work_header": "ประสบการณ์ทำงาน",
+          "education_header" :  "การศึกษา",
+          "profile":{
+            "name":"ดรัลพร สันติสวัสดิวงศ์ (มิ้นท์)",
+            "sex":"หญิง",
+            "age":"26 ปี",
+            "birthday_name":"วันเกิด",
+            "birthday":"30/01/2538"
+          },
+          "education":{
+            "university":"มหาวิทยาลัยมหิดล",
+            "faculty":"คณะเทคโนโลยีสารสนเทศและการสื่อสาร",
+            "school":"โรงเรียนวิเชียรมาตุ",
+            "mathayom":"วิทย์-คณิต",
+            "gpa_name":"เกรด"
+          },
+          "home":{
+            "start":"สวัสดี ฉันชื่อ",
+            "second":"ฉันเป็น"
+          },
+          "work_details":{
+            "position":"นักพัฒนาหน้าเว็บไซต์",
+            "first_office":"ทูบีซิมเปิ้ล",
+            "second_office":"ซอฟท์นิกซ์ เทคโนโลยี",
+          }
         }
     }
 }
