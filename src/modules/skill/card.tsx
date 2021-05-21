8558e004590d2593fea4b9dcb7713cabdc35305d
MintSoftnix <darunporn.san@gmail.com>
2021-05-21 17:08:30 +0700

    [#5]Display list and grid

--- src/modules/skill/card.tsx
+++ src/modules/skill/card.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import classnames from 'classnames'
 
 interface ICard {
+	id?:string
 	name: string;
+	data?: any[];
+	handleDisplay?:(selected:any)=>void
+	handleGrid?:(selected:any)=>void
+	list:boolean
 }
 const Card: React.FC<ICard> = (props) => {
 	const { t } = useTranslation();
-
+	console.log(props);
+	
 	return (
 		<>
 			<div className="card_skill">
 				<div className="card_ui">
 					<div className="ui_card_skill ">
 						<div className="mt-2 ml-2 mr-4 d-flex justify-content-between">
-							<div>
+							<div className="icon_card">
 								<i
 									className="material-icons mr-1"
 									style={{
@@ -42,29 +49,32 @@ const Card: React.FC<ICard> = (props) => {
 									circle
 								</i>
 							</div>
-							<div>
+							<div className="icon_card list">
 								<i
-									className="material-icons mr-1"
-									style={{
-										verticalAlign: "top",
-										fontSize: "16px",
-										color: "#75e8e7",
-									}}>
+									className="list_select material-icons px-1"
+									onClick={props.handleDisplay?.bind(this,props.id)}
+									style={!props.list?{backgroundColor:'#798B9E'}:undefined}
+
+								>
 									list
 								</i>
 								<i
-									className="material-icons"
-									style={{
-										verticalAlign: "top",
-										fontSize: "16px",
-										color: "#75e8e7",
-									}}>
+									className="list_select material-icons px-1"
+									onClick={props.handleGrid?.bind(this,props.id)}
+									style={props.list?{backgroundColor:'#798B9E'}:undefined}
+									>
 									view_module
 								</i>
 							</div>
 							<div style={{ color: "white" }}>{props.name}</div>
 						</div>
-						<div>{props.children}</div>
+						<div className="details_skill mt-4"><div className={props.list?'mx-3':''}>{props.data?.map((items:any,key:number)=>{
+							if(props.list){
+								return <GridItem icon={items.icon} name={items.name}/>
+							}else{
+								return <ListItem icon={items.icon} name={items.name}/>
+							}
+						})}</div></div>
 					</div>
 				</div>
 			</div>
@@ -72,3 +82,34 @@ const Card: React.FC<ICard> = (props) => {
 	);
 };
 export default Card;
+
+interface IListItem{
+	icon:string
+	name:string
+}
+
+const ListItem:React.FC<IListItem> = (props:any) =>{
+	return (
+		<>
+			<div className="d-flex justify-content-between mx-5">
+				<img src={props.icon} id="imgHome"  width="50px" height="50px"/>
+				<p>{props.name}</p>
+			</div>
+		</>
+	)
+
+}
+
+
+const GridItem:React.FC<IListItem> = (props:any) =>{
+	return (
+		<>
+			{/* <div className="d-flex align-items-start"> */}
+				<img src={props.icon} id="imgHome"  width="100px" height="100px"/>
+			{/* </div> */}
+		</>
+	)
+
+}
+
+
--- src/modules/skill/index.tsx
+++ src/modules/skill/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from "react";
 import { useTranslation } from 'react-i18next';
 import Card from './card'
 interface ISkill{
@@ -6,12 +7,134 @@ interface ISkill{
 }
 const Skill:React.FC<ISkill> = props =>{
     const { t } = useTranslation();
+	const [listCanDo, setListCanDo] = useState(false);
+	const [listLearning, setListLearning] = useState(false);
 
+	const handleDisplay = (list: any) => {
+		if (list === "cando") {
+			setListCanDo(false);
+		}
+		if (list === "learning") {
+			setListLearning(false);
+		}
+	};
+
+	const handleGrid = (list: any) => {
+		if (list === "cando") {
+			setListCanDo(true);
+		}
+		if (list === "learning") {
+			setListLearning(true);
+		}
+	};
     return (
 			<>
 				<div className="header_menu text-center">{t("menu_skill")}</div>
-                <div className="row"><Card name = "I can do it"/><Card name = "I am learning"/></div>
+				<div className="row card_skill_home">
+					<Card
+						id="cando"
+						name="I can do it"
+						data={ICanDoIt}
+						handleDisplay={handleDisplay}
+						handleGrid={handleGrid}
+						list={listCanDo}
+					/>
+					<Card
+						id="learning"
+						name="I am learning"
+						data={ILearning}
+						handleDisplay={handleDisplay}
+						handleGrid={handleGrid}
+						list={listLearning}
+					/>
+				</div>
 			</>
 		);
 }
 export default Skill
+
+const ICanDoIt = [
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+];
+
+const ILearning = [
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+	{
+		name: "bootstrap",
+		icon: "bootstap.png",
+	},
+];
